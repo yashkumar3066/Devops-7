ce5cda8109e618f69c9c8b31b5fbb364caeee6f4
Yash Kumar <yashkumar3066@gmail.com>
2024-09-13 17:14:23 +0530

    updated by Yash

---
+++ example.py
@@ -0,0 +1,13 @@
+#@yashkumar3066
+def sum_of_two_numbers(a, b):
+    """
+    Function to return the sum of two numbers.
+
+    Parameters:
+    a (int/float): First number.
+    b (int/float): Second number.
+
+    Returns:
+    int/float: Sum of a and b.
+    """
+    return a + b
