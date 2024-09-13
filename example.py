aedaa52200551933141f1823507b8f8c6edde614
Jahnavi <jahnavienduri@gmail.com>
2024-09-13 16:59:20 +0530

    Updated function

--- example.py
+++ example.py
@@ -1,1 +1,15 @@
-example
+#example
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+# Test the function
+number = 29
+if is_prime(number):
+    print(f"{number} is a prime number.")
+else:
+    print(f"{number} is not a prime number.")
